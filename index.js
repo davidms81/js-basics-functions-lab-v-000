0a86486a64a9b143e303ab8c3f6162ce1e9f8780
David Schoenholtz <dmschoenholtz@gmail.com>
2019-08-23 01:17:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(block){
-  
+  if (block > 42){
+    return block - 42
+  } else if (block < 42){
+    return 42 - block
+  } else {
+    return 0
+  }
 }
