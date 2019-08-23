a6e7bb5bb5332f09f6f9ceff86dd61071cfa046c
David Schoenholtz <dmschoenholtz@gmail.com>
2019-08-23 01:12:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // Code your solution in this file!
-
-function distanceFromHqInBlocks(block){
-  if (42 > block) {
-    return 42 - block;
+function distanceFromHqInBlocks(street){
+  if (street > 42){
+    return street - 42;
   } else {
-    return block - 42
+    return 42 - street;
+  }
+};
+
+function distanceFromHqInFeet(street){
+  return distanceFromHqInBlocks(street) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+  if (startBlock > endBlock){
+    return (startBlock - endBlock) * 264;
+  } else {
+    return (endBlock - startBlock) * 264;
   }
 }
 
-function distanceFromHqInFeet(block){
-  return distanceFromHqInBlocks(block) * 264
-}
-
-function distanceTravelledInFeet(start, end) {
-  if (start > end) {
-    return (start - end) * 264
-  } else {
-    return (end - start) * 264
-  }
-}
-
-function calculatesFarePrice(start, end) {
-  if (distanceTravelledInFeet(start, end) < 401) {
+function calculatesFarePrice(startBlock, endBlock){
+  let distance = distanceTravelledInFeet(startBlock, endBlock)
+  if (distance < 400){
     return 0;
-  } else if (distanceTravelledInFeet(start, end) > 400 && distanceTravelledInFeet(start, end) < 2001) {
-    return (distanceTravelledInFeet(start, end) - 400) * .02;
-  } else if (distanceTravelledInFeet(start, end) > 2000 && distanceTravelledInFeet(start, end) < 2501) {
+  } else if (distance >= 400 && distance <= 2000){
+    return (distance - 400) * .02;
+  } else if (distance > 2000 && distance < 2500){
     return 25;
   } else {
     return 'cannot travel that far'
