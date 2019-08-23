bff806bd01109f6619cbd8e40de45221dd587040
David Schoenholtz <dmschoenholtz@gmail.com>
2019-08-23 01:27:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ function distanceTravelledInFeet(startBlock, endBlock){
 }
 
 function calculatesFarePrice(startBlock, endBlock){
-  if (distanceTravelledInFeet(startBlock, endBlock) < 400){
-    return 0
-  } else if (distanceTravelledInFeet(startBlock, endBlock) > 400 && distanceTravelledInFeet(startBlock, endBlock) < 2000){
-    return (distanceTravelledInFeet(startBlock, endBlock) - 400) * .02
-  } else if (distanceTravelledInFeet(startBlock, endBlock) > 2000 && distanceTravelledInFeet(startBlock, endBlock) < 2500){
-    return 25
-  } else if (distanceTravelledInFeet(startBlock, endBlock) > 2500){
+  let distance = distanceTravelledInFeet(startBlock, endBlock)
+  if (distance < 400){
+    return 0;
+  } else if (distance >= 400 && distance <= 2000){
+    return (distance - 400) * .02;
+  } else if (distance > 2000 && distance < 2500){
+    return 25;
+  } else {
     return 'cannot travel that far'
   }
 }
